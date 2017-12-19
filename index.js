9d0e7336dc4ffe6207837da2c2d4598e4e5dcdce
Vashev <Sergey.Vashev@emc.com>
2017-12-19 19:27:37 +0300

    get temperature from other side

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    var request = require("request"),
-    cheerio = require("cheerio"),
-    url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
+app()
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => {
+        var request = require("request"),
+        cheerio = require("cheerio"),
+        url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
 
-    request(url, function (error, response, body) {
-        if (!error) {
-            var $ = cheerio.load(body),
-                temperature = $('.wu-value-to').html();
+        request(url, function (error, response, body) {
+            if (!error) {
+                var $ = cheerio.load(body),
+                    temperature = $('.wu-value-to').html();
 
-            console.log("Температура " + temperature + " градусов по Фаренгейту.111");
-        } else {
-            console.log("Произошла ошибка: " + error);
-        }
-    });
-})
+                console.log("Температура " + temperature + " градусов по Фаренгейту.111");
+            } else {
+                console.log("Произошла ошибка: " + error);
+            }
+        });
+    })
 
 app.listen(1003, () => console.log('Example app listening on port 1003!'))
 
