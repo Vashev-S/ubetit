f3df80a88116be26b0546c5a9ac32edd5ea41304
Vashev <Sergey.Vashev@emc.com>
2017-12-25 19:25:46 +0300

    bot include

--- bot/bot.js
+++ bot/bot.js
@@ -7,6 +7,9 @@ const bot = new Telegraf('442331568:AAERSNBOgyK3700GpgPYhDrh9pMHhE3N2wY')
 
 module.exports = {
     start: function () {
+        var Telegraf = require('telegraf');
+        var { reply } = Telegraf;
+        var bot = new Telegraf('442331568:AAERSNBOgyK3700GpgPYhDrh9pMHhE3N2wY');
         console.log('BOT is here');
         bot.command('/nigga', (ctx) => ctx.reply('Hello HUY!'))
         bot.command('/modern', ({ reply }) => reply('Yo'))
