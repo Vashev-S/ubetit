b32148b6a9a4d1c7d6660c9c46c30827ffe36283
Vashev <Sergey.Vashev@emc.com>
2017-12-25 19:21:04 +0300

    bot include

---
+++ bot/bot.js
@@ -0,0 +1,16 @@
+/**
+ * Bot controller
+ */
+const Telegraf = require('telegraf')
+const { reply } = Telegraf
+const bot = new Telegraf('442331568:AAERSNBOgyK3700GpgPYhDrh9pMHhE3N2wY')
+
+module.exports = {
+    start: function () {
+        console.log('BOT is here');
+        bot.command('/nigga', (ctx) => ctx.reply('Hello HUY!'))
+        bot.command('/modern', ({ reply }) => reply('Yo'))
+        bot.command('/hipster', reply('λ'))
+        bot.startPolling()
+    }
+};
--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const request = require("request")
 const cheerio = require("cheerio")
 const PORT = process.env.PORT || 5000
 
-const Telegraf = require('telegraf')
-const { reply } = Telegraf
-const bot = new Telegraf('442331568:AAERSNBOgyK3700GpgPYhDrh9pMHhE3N2wY')
+//Require
+var bot = require('./bot/bot');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -14,21 +13,16 @@ express()
         var url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
 
         request(url, function (error, response, body) {
-            if (!error) {
-                var $ = cheerio.load(body),
-                    temperature = $('.wu-value-to').html();
-
+        if (!error) {
+            var $ = cheerio.load(body),
+            temperature = $('.wu-value-to').html();
                 console.log("Температура " + temperature + " градусов по Фаренгейту.111");
             } else {
                 console.log("Произошла ошибка: " + error);
             }
         });
-
-
-
-        bot.command('/nigga', (ctx) => ctx.reply('Hello HUY!'))
-        bot.command('/modern', ({ reply }) => reply('Yo'))
-        bot.command('/hipster', reply('λ'))
-        bot.startPolling()
     })
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/botStart', (req, res) => {
+        bot.start();
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
