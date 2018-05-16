7e54ffc555b796728fdf821d77d242763fa7b08d
Vashev <Sergey.Vashev@emc.com>
2018-05-16 14:42:12 +0300

    trying to make it work

--- grabber/phantomGrab.js
+++ grabber/phantomGrab.js
@@ -71,7 +71,7 @@ module.exports = {
             }, function() {
                 console.log("The sign-in dialog should be visible now.");
                 page.evaluate(function() {
-                    return $('div.db__stats > div.db-stats__bottom > div > div:nth-child(1) > div.db-stats-table__description').text();
+                    return $('div.db__stats');
                 }).then(function(html) {
                     console.log('WOWOWOOW = ', html);
                 }).catch(function(error) {
